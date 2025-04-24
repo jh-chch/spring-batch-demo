b376fa082c4a16fc917b20b24ec85f55c9390833
jhchoi <jhchoig9@gamil.com>
2025-04-24 16:26:38 +0900

    fix: 매퍼 경로 인식 오류 수정 및 설정 변경

--- src/main/java/com/example/batch/BatchApplication.java
+++ src/main/java/com/example/batch/BatchApplication.java
@@ -1,11 +1,13 @@
 package com.example.batch;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("com.example.batch.mapper")
 public class BatchApplication {
 
 	public static void main(String[] args) {
